3a41fc01a33b09f7fb2fb0b10f5195f392c26087
alekseiLazunin <gegcuk@gmail.com>
2024-10-09 14:52:51 +0100

    feat: Enhance Spring MVC application with employee detail form handling
    - Added HTML form to `ask-emp-details-view.html` for collecting employee details with a GET request.
    - Created `show-emp-details-view.html` to display the employee name using model attributes.
    - Updated `MyController`:
      - Added `/askDetails` endpoint to display the form for entering employee details.
      - Added `/showDetails` endpoint to process submitted employee name and display the details with a greeting.
      - Demonstrated both `@RequestParam` and `HttpServletRequest` for handling form data.
    - Updated `pom.xml` to include `servlet-api` dependency for enhanced servlet support.

--- src/main/java/uk/gegc/spring/mvc/MyController.java
+++ src/main/java/uk/gegc/spring/mvc/MyController.java
@@ -1,7 +1,11 @@
 package uk.gegc.spring.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MyController {
@@ -10,4 +14,30 @@ public class MyController {
     public String showFirstView(){
         return "first-view";
     }
+
+    @RequestMapping("/askDetails")
+    public String askEmployeeDetails(){
+        return "ask-emp-details-view";
+    }
+
+    @RequestMapping("/showDetails")
+    public String showEmployeeDetails(@RequestParam("employeeName") String empName, Model model){
+        empName = "Mr. " + empName + " .";
+        model.addAttribute("nameAttribute", empName);
+        return "show-emp-details-view";
+    }
+
+//    @RequestMapping("/showDetails")
+//    public String  showEmployeeDetails(HttpServletRequest request, Model model){
+//        String empName = request.getParameter("employeeName");
+//        empName = "Mr. " + empName;
+//        model.addAttribute("nameAttribute", empName);
+//        model.addAttribute("description", "- udemy student");
+//        return "show-emp-details-view";
+//    }
+
+//    @RequestMapping("/showDetails")
+//    public String showEmployeeDetails(){
+//        return "show-emp-details-view";
+//    }
 }
--- src/main/webapp/WEB-INF/view/first-view.jsp
+++ src/main/webapp/WEB-INF/view/first-view.jsp
@@ -1,5 +1,8 @@
 <html>
 <body>
 <h2>Hello World from first-view!!!</h2>
+<br>
+<br>
+<a href="askDetails">Please rite your details</a>
 </body>
 </html>
